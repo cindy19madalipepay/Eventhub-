f1d553aa681b14d2945209256dda8a281f821a52
Cindy Madali <cindymadali04@gmail.com>
2026-08-08 14:15:22 +1000

    Fix component import casing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import { AuthProvider } from './context/AuthContext';
 import ProtectedRoute from './components/ProtectedRoute';
-import DashboardLayout from './components/Dashboardlayout';
+import DashboardLayout from './components/DashboardLayout';
 
 // Auth pages
 import Login          from './pages/auth/Login';
